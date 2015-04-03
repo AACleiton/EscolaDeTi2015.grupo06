755faa8d9479eb305c6af1e36b466f5ba88b90e5
AACleiton <aacleiton@gmail.com>
2015-04-03 03:41:26 -0300

    Alteração da url do requestmapping

--- escoladeti2015time06/src/main/java/br/unicesumar/escoladeti2015time06/ItemsDeAcesso/ItemDeAcessoController.java
+++ escoladeti2015time06/src/main/java/br/unicesumar/escoladeti2015time06/ItemsDeAcesso/ItemDeAcessoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Transactional
-@RequestMapping("/itemdeacesso/")
+@RequestMapping("/itemdeacesso")
 public class ItemDeAcessoController {
 
     @Autowired
